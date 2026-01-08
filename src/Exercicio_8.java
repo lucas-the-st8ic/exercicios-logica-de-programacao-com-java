15f907af6d0a913a419e805283623f644405a40a
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-01-08 11:23:48 -0300

    Update Exercicio_8.java

--- src/Exercicio_8.java
+++ src/Exercicio_8.java
@@ -1,3 +1,24 @@
 void main(String[] args) {
-    
+    Scanner input = new Scanner(System.in);
+
+    double preco;
+    double desconto;
+    double valorDesconto;
+    double precoFinal;
+
+    System.out.print("Qual é o preço do produto? R$");
+    preco = input.nextDouble();
+
+    System.out.println("==============================");
+
+    System.out.print("Qual será a porcentagem de desconto? % ");
+    desconto = input.nextDouble();
+
+    System.out.println("==============================");
+
+    valorDesconto = (desconto / 100) * preco;
+    precoFinal = preco - valorDesconto;
+
+    System.out.printf("O produto custava R$%.2f e agora custará R$%.2f\njá com %d%% de desconto.\n", preco, precoFinal, (int) desconto);
+    System.out.printf("Ao todo teremos R$%.2f de economia", valorDesconto);
 }
