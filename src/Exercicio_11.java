c165ef7834a5538d7dd741256a62e1eb747289ba
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-01-12 11:59:31 -0300

    Create Exercicio_11.java

---
+++ src/Exercicio_11.java
@@ -0,0 +1,13 @@
+void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+
+    String cidade;
+    int numeroDeCaracteres;
+
+    System.out.print("Em que cidade você mora?: ");
+    cidade = input.nextLine();
+
+    cidade.toUpperCase();
+    numeroDeCaracteres = cidade.length();;
+    
+}
