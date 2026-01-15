d5b937f4fb710a4717c32669bba6d2dcdb894ab1
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-01-15 15:31:35 -0300

    Create Exercicio_14.java

---
+++ src/Exercicio_14.java
@@ -0,0 +1,31 @@
+void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+
+    double valor;
+    double valorDeDesconto;
+    double desconto = 10;
+
+    System.out.print("Qual foi o valor total da compra? R$");
+    valor = input.nextDouble();
+
+    System.out.println("==========RESULTADO==========");
+    System.out.printf("Você comprou R$%.2f na nossa loja!\n", valor);
+    if (valor >= 500){
+
+        valorDeDesconto = valor * (desconto / 100.00);
+        valor -= valorDeDesconto;
+
+        System.out.println("=========ATENÇÃO==========");
+        System.out.println("Por comprar mais de R$500 reais \nvocê terá desconto em suas compras");
+        System.out.printf("Valor de desconto: R$%.2f", valorDeDesconto);
+        System.out.printf("\nValor total da sua compra: R$%.2f\n", valor);
+    } else {
+        System.out.printf("Valor total da sua compra: R$%.2f\n", valor);
+    }
+
+    System.out.println("=========OBRIGADO========");
+    System.out.println("=======VOLTE SEMPRE=======");
+
+
+
+}
