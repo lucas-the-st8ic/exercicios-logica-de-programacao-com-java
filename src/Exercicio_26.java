bd2725805b837090edb6928f23b6e637a0c19fc4
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-02-05 14:11:30 -0300

    Create Exercicio_26.java

---
+++ src/Exercicio_26.java
@@ -0,0 +1,15 @@
+import java.sql.SQLOutput;
+
+void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+
+    double primeiroOperando;
+    double segundoOperando;
+    String opcao;
+
+    System.out.println("========-TABUADA-========");
+    System.out.println("=========================");
+    System.out.println("(+ ou Adição) =>\t Soma");
+    System.out.println("(- ou Subtração) =>\t Subtração");
+    System.out.println("");
+}
