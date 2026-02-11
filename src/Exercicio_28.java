78ecad9ee6a610f4e67313f2514c3df562f26fb0
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-02-11 12:38:58 -0300

    Create Exercicio_28.java

---
+++ src/Exercicio_28.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Exercicio_28 {
+    static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        double precoProduto;
+    }
+}
