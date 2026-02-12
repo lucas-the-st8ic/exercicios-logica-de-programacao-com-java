cae197d61ea56ddd7e54a7b5f90d06a476d19f97
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-02-12 17:26:45 -0300

    updates

---
+++ src/programacao/basica/modulo03/repeticoes/RealizandoContagens.java
@@ -0,0 +1,35 @@
+package programacao.basica.modulo03.repeticoes;
+
+import java.util.Scanner;
+
+public class RealizandoContagens {
+    /*Solicita ao utilizador um valor inicial,
+    um valor final e um incremento e realiza
+    uma contagem a partir destes dados.
+    */
+    static void main(String[] args) throws InterruptedException {
+        Scanner input = new Scanner(System.in);
+
+        int numeroInicial;
+        int numeroFinal;
+        int incremento;
+
+        System.out.println("==Contagem Personalizada==");
+
+        System.out.print("Por qual valor a contagem irá começar? ");
+        numeroInicial = input.nextInt();
+
+        System.out.print("Em qual valor a contagem terminará? ");
+        numeroFinal = input.nextInt();
+
+        System.out.print("Qual vai ser o incremento? ");
+        incremento = input.nextInt();
+
+        while (numeroInicial <= numeroFinal) {
+            System.out.print(numeroInicial+ " - ");
+            Thread.sleep(500);
+            numeroInicial += incremento;
+        }
+        System.out.println("Fim");
+    }
+}
