02e596089c9cc2a29075a0f6ff2bc1917dcad2ee
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-03-07 16:17:18 -0300

    Create Exercicio_43.java

---
+++ src/programacao/basica/modulo03/repeticoes/Exercicio_43.java
@@ -0,0 +1,11 @@
+package programacao.basica.modulo03.repeticoes;
+
+import java.util.Scanner;
+
+public class Exercicio_43 {
+    static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        
+    }
+}
