9e757ef2848a9cdf8c51a3c1547a331aaaa164a7
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-03-07 16:30:25 -0300

    Update Exercicio_43.java

--- src/programacao/basica/modulo03/repeticoes/Exercicio_43.java
+++ src/programacao/basica/modulo03/repeticoes/Exercicio_43.java
@@ -6,6 +6,37 @@ public class Exercicio_43 {
     static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        
+        int numero;
+        int totalDeValores = 0;
+        int qtdNumerosPares = 0;
+        int menorValorImpar = 0;
+        String opcao = "";
+
+        do {
+            totalDeValores++;
+
+            System.out.print("Digite o " +totalDeValores+
+                    "° número:");
+            numero = input.nextInt();
+
+            System.out.print("Quer continuar? [S/N]:");
+            opcao = input.next();
+
+            if (numero % 2 == 0){
+                qtdNumerosPares++;
+
+            } else {
+                if (menorValorImpar == 0) {
+                    menorValorImpar = numero;
+
+                } else if (numero < menorValorImpar) {
+                    menorValorImpar = numero;
+                }
+            }
+
+
+        } while(opcao.equalsIgnoreCase("S"));
+
+        System.out.println("");
     }
 }
