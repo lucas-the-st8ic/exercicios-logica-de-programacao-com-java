513bbbd072f4b4af2e1236271cb7cbe05d5cef3b
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-03-09 10:13:09 -0300

    Create Exercicio_44.java

---
+++ src/programacao/basica/modulo03/repeticoes/Exercicio_44.java
@@ -0,0 +1,63 @@
+package programacao.basica.modulo03.repeticoes;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Exercicio_44 {
+    static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        Random random = new Random();
+
+        int numero;
+        int totalDeValores = 0;
+        int repeticaoNumeroCinco = 0;
+        int somaDosValores = 0;
+        int menorValor = 0;
+        int maiorValor = 0 ;
+        String opcao = "";
+
+        do {
+            totalDeValores++;
+
+            numero = random.nextInt(25) + 1;
+
+            System.out.print("O " +totalDeValores+
+                    "° valor sorteado foi: " +numero);
+
+
+            System.out.print("\nQuer continuar? [S/N]: ");
+            opcao = input.next();
+
+            if(totalDeValores == 1) {
+                menorValor = numero;
+                maiorValor = numero;
+            } else {
+                if (numero < menorValor) {
+                    menorValor = numero;
+                }
+
+                if (numero > maiorValor) {
+                    maiorValor = numero;
+                }
+            }
+
+            if (numero == 5) {
+                repeticaoNumeroCinco++;
+            }
+
+            somaDosValores += numero;
+
+        } while(opcao.equalsIgnoreCase("S"));
+
+        System.out.println("========== RESULTADOS ==========");
+        System.out.println("Ao todo foram sorteados " +totalDeValores+
+                " valores.");
+        System.out.println("A soma de todos os números sorteados foi "
+        +somaDosValores);
+        System.out.println("O menor valor foi " +menorValor+
+                " e o maior valor foi " +maiorValor);
+        System.out.println("O valor 5 foi sorteado " +repeticaoNumeroCinco+
+                " vezes.");
+        System.out.println("------------------------------------------------");
+    }
+}
